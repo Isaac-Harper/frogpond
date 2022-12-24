0a2e4b7daa8e42d03200f10600fe5b0715221cfb
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-12-24 12:08:15 -0700

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to the Lazy Frog Pond</h1>
+<img src="/jake.png" alt="jake"/>
+
+<style>
+	img {
+		width: 100vw;
+	}
+</style>
