61ec6f0f942e31201daacd11cbcec315dcd2c4ed
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-12-24 16:50:11 -0700

    padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,13 @@
 	
 <div class="page">
 	<img src="/photo.jpeg" alt="title"/>
+	
+	<div class="space"/>
 
 	<div class="flex">
 		<a href="https://www.etsy.com/shop/lazyfrogpond">
 			<Icon icon="simple-icons:etsy" />
-		</a>	
+		</a>		
 		
 		<a href="https://instagram.com/lazyfrogpond?igshid=NTdlMDg3MTY=">
 			<Icon icon="simple-icons:instagram" />
@@ -38,6 +40,10 @@
 		width: 5rem;
 		
 	}
+	
+	.space {
+		flex-grow: 1;
+	}	
 	
 	a {
 		color: black;
