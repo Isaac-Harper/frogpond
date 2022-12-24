c72a752ae0f7ca84039beac0604e92606ac7b2fa
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-12-24 16:41:16 -0700

    flex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,30 @@
-<script lang="ts">
-	import Icon from '@iconify/svelte';
-</script>
+<div class="page">
+	<script lang="ts">
+		import Icon from '@iconify/svelte';
+	</script>
 
+	<img src="/photo.jpeg" alt="title"/>
 
-
-<img src="/photo.jpeg" alt="title"/>
-
-
-
-
-<div class="flex">
-	<a href="https://www.etsy.com/shop/lazyfrogpond">
-		<Icon icon="simple-icons:etsy" />
-	</a>	
-	
-	<a href="https://instagram.com/lazyfrogpond?igshid=NTdlMDg3MTY=">
-		<Icon icon="simple-icons:instagram" />
-	</a>
+	<div class="flex">
+		<a href="https://www.etsy.com/shop/lazyfrogpond">
+			<Icon icon="simple-icons:etsy" />
+		</a>	
+		
+		<a href="https://instagram.com/lazyfrogpond?igshid=NTdlMDg3MTY=">
+			<Icon icon="simple-icons:instagram" />
+		</a>
+	</div>
 </div>
-
 
 <style>
 	img {
 		width: 100vw;
+	}
+	
+	.page {
+		display: flex;
+		direction: column;
+		align-items: center;
 	}
 	
 	.flex {
