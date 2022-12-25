45b4f556eae9ecdef43d4f50c5094cf9f9fea154
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-12-24 23:57:01 -0700

    items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,15 @@
 
 <div class="container">	
 	<div class="page">
-		<img src="/photo.jpeg" alt="title"/>
+		<img class="title" src="/photo.jpeg" alt="title"/>
 		
 		<p>Cute things made here</p>
 		
 		<div class="space"/>
+		
+		<img class="item" src="/froggies.jpg" alt="froggies"/>
+		<img class="item" src="/arson.jpg" alt="arson"/>
+		<img class="item" src="/bowl.jpg" alt="bowl"/>
 
 		<div class="flex">
 			<a href="https://www.etsy.com/shop/lazyfrogpond">
@@ -24,7 +28,7 @@
 </div>	
 
 <style>
-	img {
+	.title {
 		width: 100vw;
 	}
 	
@@ -33,10 +37,18 @@
 		justify-content: center;
 	}	
 	
+	.item {
+		width: 100%;
+		border: 4px solid #54B101;	
+		border-radius: 1rem;
+		overflow: hidden;
+	}
+	
 	.page {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		gap: 1rem;
 		
 		min-height: 100vh;
 		min-height: 100dvh;
